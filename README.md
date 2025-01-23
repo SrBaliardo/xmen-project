524583c2ec0a466ee72f8982b2cf31d4f4a51bd2
SrBaliardo <tao.john007@icloud.com>
2025-01-23 09:26:48 -0300

    feat(code): added README.

---
+++ README.md
@@ -0,0 +1,22 @@
+# Projeto X-Men para fans dos clássicos.
+
+Projeto didático <b> Dev Em Dobro </b>.
+
+Projeto realizado para prática, porém, de fácil adaptação para negócios.
+
+<br>
+
+# X-Men project for fans of the classics.
+
+<b> Dev Em Dobro </b> didactic project.
+
+Project designed for practice, but easily adapted for business.
+
+You can view the project via the link: <br>
+https://srbaliardo.github.io/xmen-project
+
+<br>
+
+" <img align="center" alt="HTML" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg"> |
+<img align="center" alt="CSS" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg"> |
+<img align="center" alt="Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg"> "
--- src/js/script.js
+++ src/js/script.js
@@ -1,41 +1,40 @@
-const chars = document.querySelectorAll('.char')
+const chars = document.querySelectorAll(".char");
 
 chars.forEach((char) => {
-    char.addEventListener('mouseenter', () => {
+  char.addEventListener("mouseenter", () => {
+    if (window.innerWidth < 450) {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
 
-        if(window.innerWidth < 450) {
-            window.scrollTo({top: 0, behavior: 'smooth'});
-        }
+    removeSelector();
 
-        removeSelector();
+    char.classList.add("selected");
 
-        char.classList.add('selected');
+    changeCharPhoto(char);
 
-        changeCharPhoto(char);
+    changeCharName(char);
 
-        changeCharName(char);
-
-        changeCharDescription(char);
-    })
-})
+    changeCharDescription(char);
+  });
+});
 
 function changeCharDescription(char) {
-    const charDescription = document.getElementById('char-description');
-    charDescription.innerText = char.getAttribute('data-description');
+  const charDescription = document.getElementById("char-description");
+  charDescription.innerText = char.getAttribute("data-description");
 }
 
 function changeCharName(char) {
-    const charName = document.getElementById('char-name');
-    charName.innerText = char.getAttribute('data-name');
+  const charName = document.getElementById("char-name");
+  charName.innerText = char.getAttribute("data-name");
 }
 
 function changeCharPhoto(char) {
-    const charPhoto = document.querySelector('.char-photo');
-    const charId = char.attributes.id.value;
-    charPhoto.src = `./src/images/${charId}.png`;
+  const charPhoto = document.querySelector(".char-photo");
+  const charId = char.attributes.id.value;
+  charPhoto.src = `./src/images/${charId}.png`;
 }
 
 function removeSelector() {
-    const selectedChar = document.querySelector('.selected');
-    selectedChar.classList.remove('selected');
+  const selectedChar = document.querySelector(".selected");
+  selectedChar.classList.remove("selected");
 }
